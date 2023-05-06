db0f92feed7607356de9fa463383d732eabb4228
sandip3022 <97385264+sandip3022@users.noreply.github.com>
2023-05-06 09:44:38 +0530

    Add files via upload

---
+++ 29ConcatenateStr.py
@@ -0,0 +1,18 @@
+'''Day 29 coding Statement : Write a Program to concatenate a string.
+Description
+Get two strings as input from the user and then concatenate it.
+Input
+
+Enter first string
+Hello
+
+Enter second string
+Hi
+
+Output
+HelloHi'''
+
+s1 = input("Enter first string")
+s2 = input("Enter second string")
+s = s1+s2
+print(s)
