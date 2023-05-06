765f855b5c16cb6f1ebdf7fdcc010b7868b1d330
sandip3022 <97385264+sandip3022@users.noreply.github.com>
2023-05-06 09:44:11 +0530

    Add files via upload

---
+++ 28ReverseStr.py
@@ -0,0 +1,13 @@
+'''Day 28 coding Statement : Write a Program to reverse a string.
+Description
+Get an input string from user and print it in reverse order.
+
+Input
+Hello
+
+Output
+olleH'''
+
+s = input()
+rev = s[::-1]  #reverse string
+print(rev)
