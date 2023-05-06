3e9f0856d6898fde312d4bfe949e08e3c4cba28f
sandip3022 <97385264+sandip3022@users.noreply.github.com>
2023-05-06 09:45:03 +0530

    Add files via upload

---
+++ 30lenghtOfString.py
@@ -0,0 +1,13 @@
+'''Day 30 coding Statement : Write a Program to print Length of the string without using strlen() function
+Description
+Get a string as input from user and print the length of the string without using strlen() function.
+
+Input
+Hello
+
+Output
+5'''
+
+s = input()
+l = len(s)
+print(l)
