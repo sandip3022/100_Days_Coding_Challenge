e7d1b094023d1c74c600561a82f303c8c5ee699c
sandip3022 <97385264+sandip3022@users.noreply.github.com>
2023-09-11 22:52:41 +0530

    Update and rename 1VowelConsonant.py to 01VowelConsonant.py

--- 01VowelConsonant.py
+++ 01VowelConsonant.py
@@ -1,3 +1,6 @@
+'''
+Day 1 Write a program to identify if the character is a vowel or consonant.
+'''
 def vowelorconsonant(x):
   if(x=='a' or x == 'e' or x == 'i' or x == 'o' or x=='u'):
     print("vowel")
