61780e089463477e777fbe33ed29b23323229a31
sandip3022 <97385264+sandip3022@users.noreply.github.com>
2023-09-11 22:47:46 +0530

    Add files via upload

---
+++ 74MinNoOfTiles.py
@@ -0,0 +1,33 @@
+"""
+Day 74 coding Statement :
+You have a grid with N rows and M columns. You have two types of tiles — one of dimensions 2×2 and the other of dimensions 1×1. You want to cover the grid using these two types of tiles in such a way that:
+Each cell of the grid is covered by exactly one tile; and The number of 1×1 tiles used is minimized. Find the minimum number of 1×1 tiles you have to use to fill the grid.
+Input Format
+The first line of input will contain a single integer T, denoting the number of test cases. Each test case consists of a single line containing two space-separated integers N,M. Output Format
+For each test case, print on a new line the minimum number of 1×1 tiles needed to fill the grid.
+
+Sample Input
+
+4
+1 1
+4 5
+6 8
+3 2
+
+Sample Output
+
+1
+4
+0
+2
+"""
+
+T = int(input())
+for i in range(T):
+  N,M = map(int,input().split())
+  if (N%2==0 and M%2==0):
+    print("0")
+  elif (N%2 !=0):
+    print(M)
+  elif (M%2 !=0):
+    print(N)
