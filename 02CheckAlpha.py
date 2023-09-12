da810cabcff645fb827eb1ff320e769606593f8d
sandip3022 <97385264+sandip3022@users.noreply.github.com>
2023-09-12 23:10:37 +0530

    Update and rename 2CheckAlpha.py to 02CheckAlpha.py

--- 02CheckAlpha.py
+++ 02CheckAlpha.py
@@ -1,3 +1,4 @@
+#Day 2 Write a program to identify if the character is an alphabet or not.
 ch = input("please enter character:")
 if((ch>='a'and ch<='z') or (ch<='A' and ch>='Z')): 
     print("The given character", ch,"is alphabet")
