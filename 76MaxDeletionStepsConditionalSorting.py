b3fbefac6b170dde5250702326cd7f018af56a14
sandip3022 <97385264+sandip3022@users.noreply.github.com>
2023-09-12 23:04:20 +0530

    Add files via upload

---
+++ 76MaxDeletionStepsConditionalSorting.py
@@ -0,0 +1,59 @@
+"""
+Day 76 coding Statement
+You are given N integers. In each step you can choose some K of the remaining numbers and delete them, if the following condition holds: 
+Let the K numbers you've chosen be a1, a2, a3, ..., aK in sorted order. Then, for each i ≤ K - 1, ai+ must be greater than or equal to ai * C.
+
+You are asked to calculate the maximum number of steps you can possibly make.
+
+Input
+
+The first line of the input contains an integer T, denoting the number of test cases. The description of each testcase follows.
+The first line of each testcase contains three integers: N, K, and C
+The second line of each testcase contains the N initial numbers
+Output
+
+For each test case output the answer in a new line.
+
+Sample Input
+
+2
+6 3 2
+4 1 2 2 3 1
+6 3 2
+1 2 2 1 4 4
+
+Sample Output
+
+1
+
+2
+"""
+
+def can_delete(nums, k, c):
+    for i in range(1, k):
+        if nums[i] < nums[i - 1] * c:
+            return False
+    return True
+
+def max_deletion_steps(N, K, C, nums):
+    left, right = 1, N
+    result = 0
+    
+    while left <= right:
+        mid = (left + right) // 2
+        if can_delete(sorted(nums[:mid]), mid, C):
+            result = mid
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return result
+
+# Read the number of test cases
+T = int(input())
+for _ in range(T):
+    N, K, C = map(int, input().split())
+    nums = list(map(int, input().split()))
+    
+    # Calculate and print the maximum number of steps
+    print(max_deletion_steps(N, K, C, nums))
